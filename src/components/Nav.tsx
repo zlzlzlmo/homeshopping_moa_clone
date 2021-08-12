153c68a9e4f8f29bc5027585fc4d9f1d67763f78
hoonVelop-shin <78241389+hoonVelop-shin@users.noreply.github.com>
2021-08-12 23:14:42 +0900

    인기검색어 띄우기

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import { search_rank } from "../data/search";
 const Nav = () => {
   const [searchBottom, setSearchBottom] = useState<string>("0%");
+  const [rankPopShow, setRankPopShow] = useState<boolean>(false);
   useEffect(() => {
     let index = 0;
     setInterval(() => {
@@ -21,22 +22,40 @@ const Nav = () => {
         <SearchInput placeholder="홈쇼핑 상품 검색" />
         <SearchIcon />
       </SearchBox>
-      <SearchRankContainer>
-        <SearchRankWrapper searchBottom={searchBottom}>
+      <SearchRankGrid
+        onMouseEnter={() => setRankPopShow(true)}
+        onMouseLeave={() => setRankPopShow(false)}
+      >
+        <SearchRankContainer>
+          <SearchRankWrapper searchBottom={searchBottom}>
+            {search_rank.map(({ word, hits }, index) => (
+              <SearchRank key={index}>
+                <div>
+                  <span className="rank">{index + 1}.</span>
+                  <span className="word">{word}</span>
+                  <span className="count">
+                    {hits}
+                    <span> hits</span>
+                  </span>
+                </div>
+              </SearchRank>
+            ))}
+          </SearchRankWrapper>
+        </SearchRankContainer>
+        <RankPopUpBox rankPopShow={rankPopShow}>
+          <div className="title">인기검색어</div>
           {search_rank.map(({ word, hits }, index) => (
-            <SearchRank key={index}>
-              <div>
-                <span className="rank">{index + 1}.</span>
-                <span className="word">{word}</span>
-                <span className="count">
-                  {hits}
-                  <span> hits</span>
-                </span>
-              </div>
-            </SearchRank>
+            <div className="query_real_time">
+              <span className="rank">{index + 1}.</span>
+              <span className="word">{word}</span>
+              <span className="count">
+                {hits}
+                <span> hits</span>
+              </span>
+            </div>
           ))}
-        </SearchRankWrapper>
-      </SearchRankContainer>
+        </RankPopUpBox>
+      </SearchRankGrid>
       <Ad>
         <img src="http://hsmoa.com/media/img/web/ad_button.png" alt="" />
       </Ad>
@@ -87,11 +106,16 @@ const SearchIcon = styled.a`
   margin: 0 10px;
 `;
 
-const SearchRankContainer = styled.div`
+const SearchRankGrid = styled.div`
   width: 20vw;
   height: 100%;
   position: relative;
+`;
+
+const SearchRankContainer = styled.div`
+  position: relative;
   overflow: hidden;
+  height: 100%;
 `;
 
 type SearchRankWrapperType = {
@@ -113,11 +137,13 @@ const SearchRank = styled.div`
   box-sizing: border-box;
   div {
     display: inline-grid;
-    grid-template-columns: 28px 120px 52px;
+    grid-template-columns: 29px 125px 54px;
     margin-right: auto;
+    position: relative;
+    padding: 5px 0;
 
     &:hover {
-      background-color: rgba(0, 0, 0, 0.2);
+      background-color: rgba(0, 0, 0, 0.1);
     }
   }
 
@@ -139,6 +165,42 @@ const SearchRank = styled.div`
   .count {
     color: gray;
     white-space: nowrap;
+  }
+`;
+
+type RankPopUpBoxType = {
+  rankPopShow: boolean;
+};
+
+const RankPopUpBox = styled.div<RankPopUpBoxType>`
+  display: ${({ rankPopShow }) => (rankPopShow ? "block" : "none")};
+  position: absolute;
+  width: 208px;
+  top: 58px;
+  box-sizing: border-box;
+  border: 1px solid black;
+  padding: 3px 6px;
+  .title {
+    font-size: 13px;
+    margin: 7px;
+    font-weight: bolder;
+  }
+  .query_real_time {
+    display: grid;
+    grid-template-columns: 2fr 5fr 3fr;
+    margin: 10px;
+    font-size: 14px;
+    padding: 3px;
+    cursor: pointer;
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.1);
+    }
+    .rank {
+      color: red;
+    }
+    .count {
+      color: grey;
+    }
   }
 `;
 
